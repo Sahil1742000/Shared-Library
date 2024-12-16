30575f9c5ebbcf7bdbd17f2adb527a817de08f91
sahil <109488746+Sahil1742000@users.noreply.github.com>
2024-12-16 23:12:19 +0530

    Update go_staticcode.groovy

--- vars/go_staticcode.groovy
+++ vars/go_staticcode.groovy
@@ -1,10 +1,31 @@
-def call() {
-         withSonarQubeEnv('SonarQube') {
-                    sh """
-                        ${SCANNER_HOME}/bin/sonar-scanner \
-                        -Dsonar.projectKey=$SONARQUBE_PROJECT_KEY \
-                        -Dsonar.sources=. \
-                        -Dsonar.host.url=${env.SONAR_HOST_URL}
-                    """
-                }
+
+
+// Function for cleaning the workspace
+def clean_workspace() {
+    cleanWs()  // Cleans the workspace before starting the build
+}
+
+// Function for installing Go dependencies
+def install_dependencies() {
+    sh 'go mod tidy'
+}
+
+// Function for code linting
+def code_linting() {
+    sh '''
+    export PATH=$(go env GOPATH)/bin:$PATH
+    golangci-lint run ./... || true
+    '''
+}
+
+// Function for SonarQube analysis
+def sonar_analysis(SCANNER_HOME, SONARQUBE_PROJECT_KEY) {
+    withSonarQubeEnv('SonarQube') {
+        sh """
+            ${SCANNER_HOME}/bin/sonar-scanner \
+            -Dsonar.projectKey=$SONARQUBE_PROJECT_KEY \
+            -Dsonar.sources=. \
+            -Dsonar.host.url=${env.SONAR_HOST_URL}
+        """
     }
+}
