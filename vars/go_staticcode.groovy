1d2bfb03a82a069a523046452f7c014d14bed397
sahil <109488746+Sahil1742000@users.noreply.github.com>
2024-12-16 23:50:47 +0530

    Update go_staticcode.groovy

--- vars/go_staticcode.groovy
+++ vars/go_staticcode.groovy
@@ -1,10 +1,10 @@
 def call() {
          withSonarQubeEnv('SonarQube') {
                     sh """
-                        sonar-scanner/bin/sonar-scanner \
-                        -Dsonar.projectKey=employee-api \
-                        -Dsonar.sources=. \
-                        -Dsonar.host.url=http://65.2.175.160:9000/
-                    """
+                       sonar-scanner \
+                       -Dsonar.projectKey=employee-api \
+                       -Dsonar.sources=. \
+                       -Dsonar.host.url=http://65.2.175.160:9000/
+                       """
                 }
 }
