9df0ac69adc702d141c0d2a3538ac3e245e610d8
sahil <109488746+Sahil1742000@users.noreply.github.com>
2024-12-17 00:15:06 +0530

    Update unitTesting.groovy

--- vars/unitTesting.groovy
+++ vars/unitTesting.groovy
@@ -1,8 +1,9 @@
 def call() {
     
         sh """
+            go mod tidy
             go test -v ./... | /var/lib/jenkins/go/bin/go-junit-report > report.xml
-            junit '**/report.xml' 
         """
+            junit '**/report.xml' 
     }
 }
